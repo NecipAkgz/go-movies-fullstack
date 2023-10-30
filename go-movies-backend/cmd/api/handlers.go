1ee8876322597fe8188467a196796b0dd4351173
NecipAkgz <nexco@hotmail.com>
2023-10-30 04:24:28 +0300

    refactor(api): replace hardcoded movie data with database query in AllMovies handler feat(api): add PostgresDBRepo implementation for DatabaseRepo interface feat(repository): define DatabaseRepo interface for interacting with the database
    The AllMovies handler in the API has been refactored to retrieve movie data from the database instead of using hardcoded data. This improves the flexibility and maintainability of the code as it allows for dynamic retrieval of movie data from the database.
    
    A new implementation of the DatabaseRepo interface, PostgresDBRepo, has been added. This implementation provides methods for connecting to the database and retrieving all movies. This allows the API to interact with the database and retrieve movie data dynamically.
    
    The addition of the DatabaseRepo interface provides a clear contract for interacting with the database, making it easier to swap out different implementations in the future if needed.

--- go-movies-backend/cmd/api/handlers.go
+++ go-movies-backend/cmd/api/handlers.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"backend/internal/models"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 // Home is the home page handler
@@ -32,33 +30,11 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 
 // AllMovies returns all movies
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
-	var movies []models.Movie
-
-	highlanderReleaseDate, _ := time.Parse("2006-01-02", "1906-03-07")
-	highlander := models.Movie{
-		ID:          1,
-		Title:       "Highlander",
-		ReleaseDate: highlanderReleaseDate,
-		MPAARating:  "R",
-		RunTime:     116,
-		Description: "An immortal Scottish swordsman must confront the last of his immortal opponents, a murderously brutal barbarian who lusts for the fabled 'Prize'",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
+	movies, err := app.DB.AllMovies()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	movies = append(movies, highlander)
-
-	inceptionReleaseDate, _ := time.Parse("2006-01-02", "2010-07-16")
-	inception := models.Movie{
-		ID:          2,
-		Title:       "Inception",
-		ReleaseDate: inceptionReleaseDate,
-		MPAARating:  "PG-13",
-		RunTime:     148,
-		Description: "A thief who enters the dreams of others to steal their secrets is offered a final job that involves planting an idea into the mind of a CEO.",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-	movies = append(movies, inception)
 
 	out, err := json.Marshal(movies)
 	if err != nil {
